756a2976cf18b39a9d268d7e19cb3e0f04fb4259
unknown <sharmachandan487@gmail.com>
2021-12-18 15:06:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,88 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import imageCompression from 'browser-image-compression';
 
 function App() {
+  const [origImg, setOrigImg] = useState('');
+  const [origImgUrl, setOrigImgUrl] = useState('');
+  const [compressedImg, setCompressedImg] = useState('');
+  const [compressedImgUrl, setCompressedImgUrl] = useState('');
+
+  const [fileName, setFileName] = useState('');
+
+  const handleImageUpload = (e) => {
+    //console.log(e.target.files[0]);
+    const image = e.target.files[0];
+    setFileName(image.name);
+    setOrigImg(image);
+    setOrigImgUrl(URL.createObjectURL(image));
+  };
+
+  const handleCompressImg = async (e) => {
+    e.preventDefault();
+
+    if (origImg / 2048 <= 0.5) {
+      alert('Upload bigger image');
+      return;
+    }
+
+    const options = {
+      maxSizeMB: 0.5,
+      maxWidthOrHeight: 500,
+      useWebWorker: true,
+    };
+
+    try {
+      const compressed = await imageCompression(origImg, options);
+
+      setCompressedImg(compressed);
+      setCompressedImgUrl(URL.createObjectURL(compressed));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="heading">React Image Compresser</div>
+      <input
+        className="upload"
+        type="file"
+        accept="image/*"
+        onChange={handleImageUpload}
+      />
+      <div className="body">
+        <div className="left">
+          {origImg && (
+            <>
+              <img className="origPic" src={origImgUrl}></img>
+            </>
+          )}
+          {origImg && (
+            <>
+              <button onClick={handleCompressImg}>Compress</button>
+            </>
+          )}
+        </div>
+
+        <div className="right">
+          {compressedImg && (
+            <>
+              <img className="origPic" src={compressedImgUrl}></img>
+            </>
+          )}
+          {compressedImg && (
+            <>
+              <button>
+                <a href={compressedImgUrl} download={fileName}>
+                  Download
+                </a>
+              </button>
+            </>
+          )}
+        </div>
+      </div>
+    </>
   );
 }
 
